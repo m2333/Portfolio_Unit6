27d396a2001b0460f2359c6cacdedb0804e6b2e0
Mei Feng Xu <m.xu@ihsph.org>
2017-01-12 14:41:31 +0000

    I changed the size of the background picture, changed the name of the project and the color for the box

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 $(document).ready(function(){
     //Write all your jQuery code here
         $("button").click(function() {
-      $("#like").html("You like this project!");
+      $(".nav").html("You like this project!");
+      $("img").attr("src", "https://www.pdx.edu/computer-science/sites/www.pdx.edu.computer-science/files/styles/pdx_collage_large/public/NB_keyboard.png")
     });
     
     
